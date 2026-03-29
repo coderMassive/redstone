d60b544ab1be27efdc8824ad902df9efba5dc2c1
Mark Raspopov <markraspopov0@gmail.com>
2026-03-28 19:59:21 -0700

    created simulator (didn't add CAL and RET support yet)

---
+++ simulator.py
@@ -0,0 +1,141 @@
+labels = {}
+flags = {"Z": 0, "C": 0, "V": 0, "N": 0}
+registers = [0]*8
+
+def process_instruction(line, pc):
+    match line[0]:
+        case "NOP":
+            pass
+        case "HLT":
+            exit()
+        case "ADD":
+            a, b = registers[int(line[1][1:])], registers[int(line[2][1:])]
+            raw = a + b
+            result = raw & 0xFF
+            flags["C"] = int(raw > 0xFF)
+            flags["V"] = int((~(a ^ b) & (a ^ result) & 0x80) != 0)
+            registers[int(line[3][1:])] = result
+        case "SUB":
+            a, b = registers[int(line[1][1:])], registers[int(line[2][1:])]
+            raw = a - b
+            result = raw & 0xFF
+            flags["C"] = int(a >= b)
+            flags["V"] = int(((a ^ b) & (a ^ result) & 0x80) != 0)
+            registers[int(line[3][1:])] = result
+        case "CMP":
+            a, b = registers[int(line[1][1:])], registers[int(line[2][1:])]
+            raw = a - b
+            result = raw & 0xFF
+            flags["C"] = int(a >= b)
+            flags["V"] = int(((a ^ b) & (a ^ result) & 0x80) != 0)
+            registers[0] = result
+        case "OR":
+            result = registers[int(line[1][1:])] | registers[int(line[2][1:])]
+            flags["C"] = 0
+            flags["V"] = 0
+            registers[int(line[3][1:])] = result
+        case "NOR":
+            result = ~(registers[int(line[1][1:])] | registers[int(line[2][1:])]) & 0xFF
+            flags["C"] = 0
+            flags["V"] = 0
+            registers[int(line[3][1:])] = result
+        case "AND":
+            result = registers[int(line[1][1:])] & registers[int(line[2][1:])]
+            flags["C"] = 0
+            flags["V"] = 0
+            registers[int(line[3][1:])] = result
+        case "XOR":
+            result = registers[int(line[1][1:])] ^ registers[int(line[2][1:])]
+            flags["C"] = 0
+            flags["V"] = 0
+            registers[int(line[3][1:])] = result
+        case "RSH":
+            flags["C"] = registers[int(line[1][1:])] & 0x01
+            result = (registers[int(line[1][1:])] >> 1) & 0xFF
+            flags["V"] = 0
+            registers[int(line[2][1:])] = result
+        case "LDI":
+            registers[int(line[1][1:])] = int(line[2])
+        case "ADI":
+            a, b = registers[int(line[1][1:])], int(line[2])
+            raw = a + b
+            result = raw & 0xFF
+            flags["C"] = int(raw > 0xFF)
+            flags["V"] = int((~(a ^ b) & (a ^ result) & 0x80) != 0)
+            registers[int(line[1][1:])] = result
+        case "INC":
+            a = registers[int(line[1][1:])]
+            b = 1
+            raw = a + b
+            result = raw & 0xFF
+            flags["C"] = int(raw > 0xFF)
+            flags["V"] = int((~(a ^ b) & (a ^ result) & 0x80) != 0)
+            registers[int(line[1][1:])] = result
+        case "DEC":
+            a = registers[int(line[1][1:])]
+            b = 1
+            raw = a - b
+            result = raw & 0xFF
+            flags["C"] = int(a >= b)
+            flags["V"] = int(((a ^ b) & (a ^ result) & 0x80) != 0)
+            registers[int(line[1][1:])] = result
+        case "JMP":
+            return int(line[1])
+        case "BRH":
+            if line[1].startswith("!"):
+                if flags[line[1][1]] == 0:
+                    return int(line[2])
+            else:
+                if flags[line[1]] == 1:
+                    return int(line[2])
+        case "CAL":
+            pass
+        case "RET":
+            pass
+    try:
+        flags["Z"] = int(result == 0)
+        flags["N"] = int(result & 0x80)
+    except:
+        pass
+    registers[0] = 0
+    return pc + 1
+
+def replace_labels(line):
+    for i, part in enumerate(line):
+        if part.startswith("."):
+            if i == 0:
+                line.pop(i) # remove label from instruction
+            else:
+                line[i] = labels[part] # replace label with address
+    return line
+
+# set labels
+with open("input.txt", 'r', encoding='utf-8') as file:
+    i = 0
+    for line in file:
+        if line.strip() == "" or line.strip().startswith("//"): # ignore empty and comment lines
+            continue
+        if line.startswith("."):
+            labels[line.split()[0]] = str(i)
+        i += 1
+
+# process instructions
+with open("input.txt", 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+
+    # remove comments and empty lines
+    for i in range(len(lines))[::-1]:
+        line = lines[i]
+        line = line.split("//")[0].strip()
+        if line == "":
+            lines.pop(i)
+        else:
+            lines[i] = line
+
+    pc = 0
+    while True:
+        line = lines[pc]
+        line = line.split()
+        line = replace_labels(line)
+        pc = process_instruction(line, pc)
+        print(registers)
