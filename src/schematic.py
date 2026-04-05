98e5cc34a41106656afa04c5dab224056bd17a3c
Mark Raspopov <markraspopov0@gmail.com>
2026-04-04 23:29:40 -0700

    updated schematic.py to support larger programs

--- src/schematic.py
+++ src/schematic.py
@@ -6,19 +6,22 @@ with open("bin/" + sys.argv[1] + ".bin", "r") as f:
 
 schem = mcschematic.MCSchematic()
 
+x = 0
 z = 0
-for line in lines:
-    x = 0
-    y = 0
 
+for line in lines:
+    y = 0
     for bit in line:
         if bit == "1":
             schem.setBlock((x, y, z), "minecraft:repeater")
         elif bit == "0":
             schem.setBlock((x, y, z), "minecraft:light_blue_stained_glass")
-
         y -= 2
 
-    z -= 6
+    if z <= -90:
+        z = 0
+        x -= 2
+    else:
+        z -= 6
 
 schem.save("schem", sys.argv[1], mcschematic.Version.JE_1_18_2)
